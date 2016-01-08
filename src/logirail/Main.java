c86fc655bbfad9929dfae09788c57b7a408cac17
uj <sudo.screenlife@gmail.com>
2016-01-08 17:57:01 +0100

    Added sample javadoc

--- src/logirail/Main.java
+++ src/logirail/Main.java
@@ -1,7 +1,14 @@
 package logirail;
 
+/*
+ * Main d'exemple, qui affiche un hello world. 
+ * @author Julien ABADJI
+ * */
 public class Main {
-
+	/*
+	 * Fonction statique main, qui permet de lancer du code tranquillement. 
+	 * @param args Arguments passés en ligne de commande. 
+	 * */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world !");
