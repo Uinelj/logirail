ce418af8251d3d08e7e349f6426fc2b292bc88a9
aktor <aktor.alexis@hotmail.fr>
2016-02-09 11:13:19 +0100

    implémentation du constructeur de MissionCodeDataBase + javadoc

---
+++ src/data/MissionCodeDatabase.java
@@ -0,0 +1,18 @@
+package data;
+
+/**
+ * Class MissionCodeDatabase : dataBase of all codeMissions
+ * @author AKTOR Alexis
+ * 
+ */
+
+import java.util.ArrayList;
+
+public class MissionCodeDatabase {
+	ArrayList<MissionCode> dataBase;
+	
+	public MissionCodeDatabase(){
+		dataBase = new ArrayList<MissionCode>();
+	}
+
+}
