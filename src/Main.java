4c851c87b7854d8fbe665e15cf24a9a61e5dd9c2
AlexFourgs <Alexandre.Fourgs@outlook.fr>
2016-02-11 23:15:19 +0100

    [Modifications] Déplacement de gare.json dans le dossier data + Maj main.

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		GareDataBase gare = new GareDataBase();
-		gare.LoadJsonGare("gare.json");
+		gare.LoadJsonGare("../data/gare.json");
 		System.out.println(gare);
 	}
 
