2e5d076d2de607af590e13712bf9d02c20de1abb
aryus96 <aryus96@hotmail.fr>
2016-02-12 16:15:28 +0100

    Modification of graphics librairies and new test of HIM

--- src/Graphics/FirstView.java
+++ src/Graphics/FirstView.java
@@ -8,9 +8,12 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ProgressIndicator;
@@ -19,16 +22,18 @@ import javafx.stage.Stage;
 import javafx.util.Duration;
 
 public class FirstView extends Application{
-	
+
 	public static void main (String[] args){
 		Application.launch(FirstView.class,args);
 	}
-	
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Logirail la lourdeur");
-		Group root = new Group();
-		Scene scene = new Scene(root,300,250,Color.ALICEBLUE);
+		//Group root = new Group();
+	    Parent root = FXMLLoader.load(getClass().getResource("./main.fxml"));
+
+		Scene scene = new Scene(root,1280,640,Color.ALICEBLUE);
 		Button btn = new Button();
 		btn.setLayoutX(100);
 		btn.setLayoutY(80);
@@ -38,35 +43,34 @@ public class FirstView extends Application{
 				System.out.println("Wsh frr tu me clik dsssu la");
 			}
 		});
-		
+
 		JFXButton jfoenixButton = new JFXButton("JFoenix Button");
-		
+
+
 		JFXButton button = new JFXButton("Raised Button".toUpperCase());
 		button.getStyleClass().add("button-raised");
-		
+		button.setLayoutX(100);
+
 		JFXCheckBox checkBox = new JFXCheckBox("JFX CheckBox");
 		checkBox.getStyleClass().add("custom-jfx-check-box");
-		
-		
-		JFXProgressBar jfxBar = new JFXProgressBar();
-		jfxBar.setPrefWidth(500);
-		JFXProgressBar jfxBarInf = new JFXProgressBar();
-		jfxBarInf.setPrefWidth(500);
-		jfxBarInf.setProgress(-1.0f);
-		Timeline timeline = new Timeline(
-		    new KeyFrame(Duration.ZERO, new KeyValue(jfxBar.progressProperty(), 0), new KeyValue(jfxBar.progressProperty(), 0)),
-		    new KeyFrame(Duration.seconds(2), new KeyValue(jfxBar.progressProperty(), 1), new KeyValue(jfxBar.progressProperty(), 1)));
-		timeline.setCycleCount(Timeline.INDEFINITE);
-		timeline.play();
-		
-		root.getChildren().add(button);
-		root.getChildren().add(checkBox);
-		root.getChildren().add(btn);
+		checkBox.setLayoutY(200);
+		//primaryStage.minWidthProperty().bind(scene.heightProperty().multiply(2));
+		//primaryStage.minHeightProperty().bind(scene.widthProperty().divide(4));
+
+		 primaryStage.titleProperty().bind(
+		            scene.widthProperty().asString().
+		            concat(" : ").
+		            concat(scene.heightProperty().asString()));
+
+		//root.getChildren().add(button);
+		//root.getChildren().add(checkBox);
+		//root.getChildren().add(btn);
 		primaryStage.setScene(scene);
 		primaryStage.setResizable(true);
+		primaryStage.sizeToScene();
 		primaryStage.show();
-		
-		
+
+
 	}
-	
+
 }
