9ab5930d362759f70a4dd9a21c5e2c75560a8223
uj <aulien.jbadjigmail.com>
2016-02-12 15:27:19 +0000

    added iterator for missionCodeDatabase

--- src/data/MissionCodeDatabase.java
+++ src/data/MissionCodeDatabase.java
@@ -3,30 +3,41 @@ package data;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Class MissionCodeDatabase : dataBase of all codeMissions
  * @author AKTOR Alexis
  * 
  */
-import Json.JSONArray;
-import Json.JSONObject;
-import Json.JSONParser;
-import Json.ParseException;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class MissionCodeDatabase {
-	ArrayList<MissionCode> dataBase;
+	HashMap<String, ArrayList<String>> missionCodeDatabase;
 	
 	public MissionCodeDatabase(){
 		final JSONParser parser = new JSONParser();
+		JSONObject jsonObject;
 		try {
-			final JSONObject jsonObject =  (JSONObject) parser.parse(new FileReader("data/missioncodes.json"));
-			System.out.println(jsonObject.toString());
-			
+			jsonObject = (JSONObject) parser.parse(new FileReader("data/missioncodes.json"));
+			//TODO : Adapter cela :)
+			/*public void populateResult(){
+				jo = new JSONObject(jsonAnswer);
+				Iterator<String> keysItr = jo.keys();
+				while(keysItr.hasNext()){
+					String key = keysItr.next();
+					String value = jo.get(key).toString();
+					result.put(key, value);
+				}
+			}*/
+
 		} catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		
 	}
 
