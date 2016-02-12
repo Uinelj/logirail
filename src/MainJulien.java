c5bcd386e8042b212d85ef1d6ff33057029ce2e6
uj <sudo.screenlife@gmail.com>
2016-02-12 20:46:19 +0100

    It works ! Lacks quality and singleton pattern.

--- src/MainJulien.java
+++ src/MainJulien.java
@@ -5,6 +5,9 @@ public class MainJulien {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		MissionCodeDatabase mcd = new MissionCodeDatabase();
+		System.out.println(mcd.getRoute("BILE"));
+		System.out.println(mcd.getRoute("TEST"));
+		System.out.println(mcd.getRoute("ARYA"));
 		
 	}
 }
--- src/data/MissionCodeDatabase.java
+++ src/data/MissionCodeDatabase.java
@@ -4,42 +4,67 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Set;
 
-/**
- * Class MissionCodeDatabase : dataBase of all codeMissions
- * @author AKTOR Alexis
- * 
- */
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 public class MissionCodeDatabase {
-	HashMap<String, ArrayList<String>> missionCodeDatabase;
+	//TODO : SafetyChecks, clean the code.
+	//TODO : Make it a singleton.
+	//TODO : Don't hardcode missioncodedatabase, road and name.
+	//TODO : Does the missionCodeDatabase has to contain Gares ? 
+	private String dbPath;
+	private HashMap<String, ArrayList<Integer>> missionCodeDatabase = new HashMap<String, ArrayList<Integer>>();
+
+	public MissionCodeDatabase(String path){
+		dbPath = path;
+		parseJson(path);
+	}
 	
 	public MissionCodeDatabase(){
+		this("data/missioncodes.json");
+	}
+	
+	private void parseJson(String path){
+		
 		final JSONParser parser = new JSONParser();
-		JSONObject jsonObject;
+		JSONObject jsonObject, missionCode;
+		JSONArray road;
+		ArrayList<Integer> currentMissionCodeRoad = new ArrayList<Integer>();
+		
 		try {
-			jsonObject = (JSONObject) parser.parse(new FileReader("data/missioncodes.json"));
-			//TODO : Adapter cela :)
-			/*public void populateResult(){
-				jo = new JSONObject(jsonAnswer);
-				Iterator<String> keysItr = jo.keys();
-				while(keysItr.hasNext()){
-					String key = keysItr.next();
-					String value = jo.get(key).toString();
-					result.put(key, value);
-				}
-			}*/
-
+			jsonObject = (JSONObject) parser.parse(new FileReader(path));
+			jsonObject = (JSONObject) jsonObject.get("missioncodedatabase");
+			
+			for(String curKey : (Set<String>) jsonObject.keySet()){
+				missionCode = (JSONObject) jsonObject.get(curKey);
+				road = (JSONArray) missionCode.get("road");
+				if (road != null) { 
+					   for (int i=0;i<road.size();i++){ 
+					    currentMissionCodeRoad.add(Integer.parseInt(road.get(i).toString()));
+					   } 
+					}
+				//System.out.println(currentMissionCodeRoad);
+				//System.out.println(missionCode.get("name").toString());
+				missionCodeDatabase.put(missionCode.get("name").toString(), (ArrayList<Integer>) currentMissionCodeRoad.clone());
+				currentMissionCodeRoad.clear();
+			}
+			//System.out.println(missionCodeDatabase);
+			
 		} catch (IOException | ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
-
+	
+	
+	public void reloadJson(){
+		parseJson(dbPath);
+	}
+	public ArrayList<Integer> getRoute(String missionCode){
+		return missionCodeDatabase.get(missionCode);
+	}
 }
