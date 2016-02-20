8f35389b1adb088351e389b61c3f080f66cb9d06
uj <aulien.jbadji@gmail.com>
2016-02-20 17:13:52 +0100

    modified some typos in gare.json

--- src/MainJulien.java
+++ src/MainJulien.java
@@ -1,15 +1,20 @@
 import data.GareDataBase;
+import data.MissionCode;
 import data.MissionCodeDatabase;
 
 public class MainJulien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		/*MissionCodeDatabase mcd = MissionCodeDatabase.getInstance();
-		System.out.println(mcd.getMissionCode("TEDI").toString());
+		MissionCodeDatabase mcd = MissionCodeDatabase.getInstance();
+		/*System.out.println(mcd.getMissionCode("TEDI").toString());
 		System.out.println(mcd.getMissionCode("ZEBU"));
 		System.out.println(mcd.getMissionCode("NAGA"));*/
 		GareDataBase gdb = GareDataBase.getInstance();
-		System.out.println(gdb.getGare(17));
+		MissionCode tedi = mcd.getMissionCode("TEDI");
+		for(Integer i : tedi.getRoad()){
+			System.out.println(gdb.getGare(i).getName() + "\n");
+		}
+		//System.out.println(gdb.getGare(17).getName());
 	}
 }
