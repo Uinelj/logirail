78fbd72c539d9faa1f67ab6ecc67ae51ed8a5b53
uj <aulien.jbadji@gmail.com>
2016-03-18 15:16:06 +0100

    Added gradle config files.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import data.MissionCode;
 import data.MissionCodeDatabase;
 import data.ScheduleDatabase;
 
-public class MainJulien {
+public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
