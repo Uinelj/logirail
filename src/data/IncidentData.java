20dc3fae77416b98df21caed4166be79bf013dd9
AlexFourgs <Alexandre.Fourgs@outlook.fr>
2016-03-29 09:57:57 +0200

    Add/Create Incident Data

---
+++ src/data/IncidentData.java
@@ -0,0 +1,37 @@
+package data;
+
+public class IncidentData {
+	private int id ;
+	private String type ;
+	private int minTime ;
+	private int maxTime ;
+	
+	public IncidentData(int id, String type, int minTime, int maxTime){
+		this.id = id ;
+		this.type = type ;
+		this.minTime = minTime ;
+		this.maxTime = maxTime ;
+	}
+	
+	public int getId(){
+		return id ;
+	}
+	
+	public String getType(){
+		return type ;
+	}
+	
+	public int getMinTime(){
+		return minTime ;
+	}
+	
+	public int getMaxTime(){
+		return maxTime ;
+	}
+	
+	public String toString(){
+		String toString = "[Incident] ID : " + id + " Type : "+ type + " minTime : " + minTime + " maxTime : " + maxTime ;
+		
+		return toString ;
+	}
+}
